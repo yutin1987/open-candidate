7abca0de963de0d4dade336c8b78db7d22812b09
YuTin <yuting1987@gmail.com>
2018-09-15 12:39:54 +0800

    feat(map): first map

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import dynamic from 'next/dynamic'
+
+const Game = dynamic(import('../components/Game/Game'));
+
+export default class extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Game />
+      </React.Fragment>
+    );
+  }
+}
